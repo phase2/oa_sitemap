c53d8e5858c2b4c86c74248425483b7a96f48eee
Mike Potter <mpotter@phase2technology.com>
2014-09-10 13:14:49 -0400

    Added "New Section" and "New Space"

--- templates/oa-sitemap-show.tpl.php
+++ templates/oa-sitemap-show.tpl.php
@@ -11,14 +11,10 @@
  */
 ?>
 <div class="oa_space_wrapper" data-depth="<?php print $depth; ?>" data-index="<?php print $index; ?>">
-  <?php if (!empty($node)): ?>
-    <h3 class="oa_space_title"><?php print l($node->title, 'node/' . $node->nid); ?></h3>
-  <?php else: ?>
-    <h3 class="oa_space_title"><?php print l(t('Home'), '<front>'); ?></h3>
-  <?php endif; ?>
+  <h3 <?php print (!empty($class)) ? 'class = "' . $class . '"' : ''; ?>><?php print $link; ?></h3>
   <div class="oa_section_wrapper">
   <?php foreach ($sections as $section): ?>
-    <div class="oa_section"><h4 class="oa_section_title"><?php print $section->icon; ?><?php print l($section->title, 'node/' . $section->nid); ?></h4></div>
+    <div class="oa_section"><h4 class="oa_section_title"><?php print $section['icon']; ?><?php print $section['link']; ?></h4></div>
   <?php endforeach; ?>
   </div>
   <div class="oa_subspace_wrapper">
