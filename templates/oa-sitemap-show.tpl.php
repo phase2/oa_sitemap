4e54ab3be0862b39efb98f4dbd51ddda7e791204
Mike Potter <mpotter@phase2technology.com>
2014-09-10 13:18:43 -0400

    Added Space icons

--- templates/oa-sitemap-show.tpl.php
+++ templates/oa-sitemap-show.tpl.php
@@ -11,10 +11,10 @@
  */
 ?>
 <div class="oa_space_wrapper" data-depth="<?php print $depth; ?>" data-index="<?php print $index; ?>">
-  <h3 <?php print (!empty($class)) ? 'class = "' . $class . '"' : ''; ?>><?php print $link; ?></h3>
+  <h3 <?php print !empty($class) ? 'class = "' . $class . '"' : ''; ?>><?php print !empty($icon) ? $icon : ''; ?><?php print $link; ?></h3>
   <div class="oa_section_wrapper">
   <?php foreach ($sections as $section): ?>
-    <div class="oa_section"><h4 class="oa_section_title"><?php print $section['icon']; ?><?php print $section['link']; ?></h4></div>
+    <div class="oa_section"><h4 class="oa_section_title"><?php print !empty($section['icon']) ? $section['icon'] : ''; ?><?php print $section['link']; ?></h4></div>
   <?php endforeach; ?>
   </div>
   <div class="oa_subspace_wrapper">
