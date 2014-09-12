6c4ea7187201860c8c5fa2a17b2fa958e60629e1
Mike Potter <mpotter@phase2technology.com>
2014-09-12 10:39:08 -0400

    Added Space selector dropdown and header template

---
+++ templates/oa-sitemap-header.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Header template for Open Atrium sitemap.
+ * This template is used for the top header of the sitemap
+ * $link - link for full sitemap
+ * $search - search dropdown list
+ */
+?>
+<div class="oa_sitemap_header clearfix">
+  <div class="oa_sitemap_search pull-right"><?php print $search; ?></div>
+  <div class="oa_sitemap_full pull-left"><?php print $link; ?></div>
+</div>
