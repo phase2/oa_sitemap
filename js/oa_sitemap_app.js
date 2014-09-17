6929f3077f3553304c6bc0036b04efcc1b326d97
Micah Godbolt <mgodbolt@phase2technology.com>
2014-09-17 16:46:47 -0700

    Subspace cleanup, moved icon location to inside of object to specify order by key

--- js/oa_sitemap_app.js
+++ js/oa_sitemap_app.js
@@ -17,23 +17,23 @@
       var spaces = settings.oa_sitemap.spaces;
       var breadcrumbs = [];
 
+      console.log(topID);
+      console.log(spaces);
+
       function loadSpace(id) {
         var parentId = spaces[id].parent_id;
         var currentSpaces = [];
-
-        for(var i in spaces) {
-          if (spaces[i].parent_id == parentId && spaces[i].nid != 0) {
-            var tempSpace = spaces[i];
-            var tempArray = [];
-            for(var t in spaces) {
-              if (spaces[t].parent_id == spaces[i].nid) {
-                tempArray.push(spaces[t]);
-              }
-            }
-            spaces[i].subspaces = tempArray;
-            currentSpaces.push(tempSpace);
-          };
+        var parentSpace = "";
+        if (parentSpace = spaces[parentId]) {
+          for(var i in parentSpace.subspaces) {
+            var childSpaceId = parentSpace.subspaces[i];
+            currentSpaces.push(spaces[childSpaceId]);
+          }
         }
+        else {
+          currentSpaces.push(spaces[id]);
+        }
+
         return currentSpaces;
       }
 
@@ -41,7 +41,7 @@
           var parentId = spaces[id].parent_id;
           var breadcrumb = spaces[parentId];
 
-          if (spaces[parentId].nid != 0) {
+          if (spaces[id].nid != 0) {
             breadcrumbs.push(breadcrumb);
             loadBreadCrumbs(parentId);
           }
@@ -61,8 +61,9 @@
       var app = angular.module("oaSitemap", ['ngSanitize']);
 
       app.controller("oaSitemapController", function($scope, $http) {
-        $scope.spaces = loadSpace(0);
-        $scope.breadcrumbs = loadBreadCrumbs(0);
+        $scope.allSpaces = spaces;
+        $scope.spaces = loadSpace(topID);
+        $scope.breadcrumbs = loadBreadCrumbs(topID);
         $scope.icons = icons;
         $scope.currentSlide = 0;
 
--- templates/oa-sitemap-app.tpl.php
+++ templates/oa-sitemap-app.tpl.php
@@ -15,30 +15,53 @@
   <ul class="oa-sitemap-breadcrumbs">
     <li class="oa-sitemap-breadcrumb" ng-repeat="breadcrumb in breadcrumbs.slice().reverse()"><a ng-click='explore(breadcrumb.nid)'>{{breadcrumb.title}}</a></li>
   </ul>
-
-  <div class="oa-space-header">
-    <button class="prev" ng-show="spaces[currentSlide - 1]" ng-click="currentSlide = currentSlide -1">{{spaces[currentSlide - 1].title}}</button>
-    <div class="dropdown">
-      <a class="oa-space-title" data-toggle="dropdown" href="#">{{spaces[currentSlide].title}} nid is:{{spaces[currentSlide].nid}}</a>
-      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-        <li ng-repeat="space in spaces"><a ng-click="slide($index)">{{space.title}}</a></li>
-      </ul>
+  <div class="oa-carousel-container">
+    <div class="oa-space-header">
+      <button class="prev" ng-show="spaces[currentSlide - 1]" ng-click="currentSlide = currentSlide -1">{{spaces[currentSlide - 1].title}}</button>
+      <div class="dropdown">
+        <a class="oa-space-title" data-toggle="dropdown" href="#">{{spaces[currentSlide].title}}</a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+          <li ng-repeat="space in spaces"><a ng-click="slide($index)">{{space.title}}</a></li>
+        </ul>
+      </div>
+      <button class="next" ng-show="spaces[currentSlide + 1]" ng-click="currentSlide = currentSlide +1">{{spaces[currentSlide + 1].title}}</button>
     </div>
-    <button class="next" ng-show="spaces[currentSlide + 1]" ng-click="currentSlide = currentSlide +1">{{spaces[currentSlide + 1].title}}</button>
-  </div>
-  <div class="oa-spaces" ng-class="{active: $index == currentSlide}" ng-repeat="space in spaces">
-    <section class="oa-sections">
-      <div class="oa-section" ng-repeat="section in space.sections">
-        <h4 class="oa-section-title"><div class="oa-section-icon" ng-bind-html="icons[section.icon_id]"></div><a href="{{section.url}}" class="oa-section-link {{section.class}}">{{section.title}}</a></h4>
-      </div>
-    </section>
 
-    <section class="oa-subspaces">
-      <div class="oa-subspace" ng-repeat="subspace in space.subspaces">
-        <h4 class="oa-subspace-title"><a ng-click='explore(subspace.nid)' class="oa-subspace-link"><span>{{subspace.title}}</span></a></h4>
-      </div>
-    </section>
+    <div class="oa-spaces" ng-class="{active: $index == currentSlide}" ng-repeat="space in spaces">
+    {{$parent.spaces[6]}}
+      <section class="oa-sections">
+        <div class="oa-section" ng-repeat="section in space.sections">
+          <h4 class="oa-section-title"><div class="oa-section-icon" ng-bind-html="icons[section.icon_id]"></div><a href="{{section.url}}" class="oa-section-link {{section.class}}">{{section.title}}</a></h4>
+        </div>
+      </section>
 
+      <section class="oa-subspaces">
+        <div class="oa-subspace" ng-repeat="index in space.subspaces">
+          <div class="oa-subspace-icons">
+
+            <div ng-repeat="icon in $parent.$parent.allSpaces[index].icons| orderBy:orderIcons" ng-class="[icon.class, 'oa-subspace-icon-' + icon.position ]">
+              <a ng-href="{{icon.url}}">
+                <i ng-class="[icon.icon, icon.class]"></i>
+              </a>
+
+            </div>
+          </div>
+          <h4 class="oa-subspace-title">
+            <a ng-click='explore($parent.allSpaces[index].nid)' class="oa-subspace-link">
+              <span>{{$parent.allSpaces[index].title}}</span>
+            </a>
+          </h4>
+        </div>
+        <div class="oa-subspace oa-new-space">
+          <h4 class="oa-subspace-title">
+            <a href="/wizard/nojs/add/oa-space" class="oa-subspace-link ctools-use-modal ctools-modal-oa-sitemap-space ctools-use-modal-processed" title="New Space">
+              <span><i class="icon-plus"></i>New Space</span>
+            </a>
+          </h4>
+        </div>
+      </section>
+
+    </div>
   </div>
 
 </div>
