3dc4cbb626f064be8db439e489a0c0c960edeaa8
Micah Godbolt <mgodbolt@phase2technology.com>
2014-09-16 17:05:12 -0700

    All the angular things

--- js/oa_sitemap_app.js
+++ js/oa_sitemap_app.js
@@ -14,41 +14,60 @@
       var topID = settings.oa_sitemap.topID;
       var icons = settings.oa_sitemap.icons;
       var data = settings.oa_sitemap.data;
+      var spaces = settings.oa_sitemap.spaces;
+      var breadcrumbs = [];
 
-      function searchObj( obj , id ){
+      function loadSpace(id) {
+        var parentId = spaces[id].parent_id;
+        var currentSpaces = [];
 
-          for( var i = 0; i < obj.subspaces.length; i++ ) {
-            if (obj.subspaces[i].index == id) {
-              return obj.subspaces[i].subspaces;
+        for(var i in spaces) {
+          if (spaces[i].parent_id == parentId && spaces[i].nid != 0) {
+            var tempSpace = spaces[i];
+            var tempArray = [];
+            for(var t in spaces) {
+              if (spaces[t].parent_id == spaces[i].nid) {
+                tempArray.push(spaces[t]);
+              }
             }
-            if (obj.subspaces[i].subspaces.length > 1) {
-              return searchObj(obj.subspaces[i], id);
-            };
+            spaces[i].subspaces = tempArray;
+            currentSpaces.push(tempSpace);
+          };
+        }
+        return currentSpaces;
+      }
+
+      function loadBreadCrumbs(id) {
+          var parentId = spaces[id].parent_id;
+          var breadcrumb = spaces[parentId];
+
+          if (spaces[parentId].nid != 0) {
+            breadcrumbs.push(breadcrumb);
+            loadBreadCrumbs(parentId);
           }
+
+          return breadcrumbs;
       }
 
 
-      var app = angular.module("oaSitemap", ['ui.bootstrap']);
+
+      var app = angular.module("oaSitemap", ['ui.bootstrap','ngSanitize']);
 
       app.controller("oaSitemapController", function($scope, $http) {
-        $scope.spaces = data.subspaces;
-        $scope.breadcrumbs = [
-        {'title': 'North',
-         'link' : '/sitemap_app/2'},
-        {'title': 'North West',
-         'link' : '/sitemap_app/3'}
-         ];
+        $scope.spaces = loadSpace(613);
+        $scope.breadcrumbs = loadBreadCrumbs(613);
+        $scope.icons = icons;
         $scope.explore = function(index) {
-
-          $scope.spaces = searchObj(data,index);
-
+          breadcrumbs = [];
+          $scope.breadcrumbs = loadBreadCrumbs(index);
+          $scope.spaces = loadSpace(index);
         };
-        console.log(data);
+        console.log(breadcrumbs);
+        console.log(spaces);
       });
 
 
-
-      // console.log(icons);
+       console.log(icons);
 
 
     }
--- templates/oa-sitemap-app.tpl.php
+++ templates/oa-sitemap-app.tpl.php
@@ -12,23 +12,27 @@
     <div class="oa-sitemap-search pull-right"><?php print $search; ?></div>
     <div class="oa-sitemap-full pull-left"><?php print $link; ?></div>
   </div>
-  <ul class="oa-breadcrumbs" ng-repeat="breadcrumb in breadcrumbs">
-    <li class="oa-breadcrumb"><a href="{{breadcrumb.link}}">{{breadcrumb.title}}</a></li>
+  <ul class="oa-sitemap-breadcrumbs">
+    <li class="oa-sitemap-breadcrumb" ng-repeat="breadcrumb in breadcrumbs.slice().reverse()"><a ng-click='explore(breadcrumb.nid)'>{{breadcrumb.title}}</a></li>
   </ul>
   <carousel>
-    <slide class="oa-spaces" ng-repeat="space in spaces">
+    <slide class="oa-spaces" ng-repeat="space in spaces" active="slide.active">
 
-      <h2 class="oa-space-title">{{space.title}}</h2>
+      <h2 class="oa-space-title">
+        <button ng-click="next()">Previous</button>
+        {{space.title}}
+        <button ng-click="next()">Next</button>
+      </h2>
 
       <section class="oa-sections">
         <div class="oa-section" ng-repeat="section in space.sections">
-          <h3 class="oa-section-title"><a href="{{section.url}}" class="oa-section-link">{{section.title}}</a></h3>
+          <h3 class="oa-section-title"><div class="oa-section-icon" ng-bind-html="icons[section.icon_id]"></div><a href="{{section.url}}" class="oa-section-link {{section.class}}">{{section.title}}</a></h3>
         </div>
       </section>
 
       <section class="oa-subspaces">
         <div class="oa-subspace" ng-repeat="subspace in space.subspaces">
-          <h3 class="oa-subspace-title"><a ng-click='explore(subspace.index)' class="oa-subspace-link">{{subspace.title}}</a></h3>
+          <h3 class="oa-subspace-title"><a ng-click='explore(subspace.nid)' class="oa-subspace-link"><span>{{subspace.title}}</span></a></h3>
         </div>
       </section>
 
